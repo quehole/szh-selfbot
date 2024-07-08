eeaa81a22ab94e7e3fbe966c01fc8ef1d732ba83
Ruler <148696064+quehole@users.noreply.github.com>
2024-07-08 11:25:48 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  token: "YOUR TOKEN",
+
+  prefix: ".",
+
+  allowedUserIDs: ["YOUR DISCORD USER ID"],
+
+  channelId: "CHANNEL ID",  // Add your channel ID where the auto message will go.
+
+  messageToSend: "AUTO MESSAGE CONTENTS"
+
+};
